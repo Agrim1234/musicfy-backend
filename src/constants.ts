3779c2600625b99284ca6ce2560db695f7a59ee7
Agrim1234 <agrimagarwal123456789@gmail.com>
2024-11-06 23:16:22 +0530

    minor

--- src/constants.ts
+++ src/constants.ts
@@ -8,102 +8,102 @@ export const tagFileData = [
     {
         id: '1',
         tagName: 'joyful',
-        fileName: 'public/joyride.mp3'
+        fileName: 'uploads/joyride.mp3'
     },
     {
         id: '2',
         tagName: 'jazz',
-        fileName: 'public/jazz.mp3'
+        fileName: 'uploads/jazz.mp3'
     },
     {
         id: '3',
         tagName: 'contemporary pop/rock',
-        fileName: 'public/puppy-love.mp3'
+        fileName: 'uploads/wandering.mp3'
     },
     {
         id: '4',
         tagName: 'drums (drum set)',
-        fileName: 'public/drums.mp3'
+        fileName: 'uploads/drums.mp3'
     },
     {
         id: '5',
         tagName: 'blues rock',
-        fileName: 'public/jazz.mp3'
+        fileName: 'uploads/jazz.mp3'
     },
     {
         id: '6',
         tagName: 'hard rock',
-        fileName: 'public/hard-rock.mp3'
+        fileName: 'uploads/hard-rock.mp3'
     },
     {
         id: '7',
         tagName: 'pop rock',
-        fileName: 'public/joyride.mp3'
+        fileName: 'uploads/joyride.mp3'
     },
     {
         id: '8',
         tagName: 'electro-swing',
-        fileName: 'public/electro-swing.mp3'
+        fileName: 'uploads/electro-swing.mp3'
     },
     {
         id: '9',
         tagName: 'bass guitar',
-        fileName: 'public/bass.mp3'
+        fileName: 'uploads/bass.mp3'
     },
     {
         id: '10',
         tagName: 'rock',
-        fileName: 'public/hard-rock.mp3'
+        fileName: 'uploads/hard-rock.mp3'
     },
     {
         id: '11',
         tagName: 'soft rock',
-        fileName: 'public/hard-rock.mp3'
+        fileName: 'uploads/hard-rock.mp3'
     },
     {
         id: '12',
         tagName: 'guitar',
-        fileName: 'public/bass.mp3'
+        fileName: 'uploads/bass.mp3'
     },
     {
         id: '13',
         tagName: 'pop-metal',
-        fileName: 'public/joyride.mp3'
+        fileName: 'uploads/joyride.mp3'
     },
     {
         id: '14',
         tagName: 'pop',
-        fileName: 'public/pop.mp3'
+        fileName: 'uploads/pop.mp3'
     },
     {
         id: '15',
         tagName: 'rock ballad',
-        fileName: 'public/hard-rock.mp3'
+        fileName: 'uploads/hard-rock.mp3'
     },
     {
         id: '16',
         tagName: 'rock and roll',
-        fileName: 'public/bass.mp3'
+        fileName: 'uploads/bass.mp3'
     },
     {
         id: '17',
         tagName: 'electric guitar',
-        fileName: 'public/guitarelectric.mp3'
+        fileName: 'uploads/guitarelectric.mp3'
     },
     {
         id: '18',
         tagName: 'prog-rock',
-        fileName: 'public/guitar.mp3'
+        fileName: 'uploads/guitar.mp3'
     },
     {
         id: '19',
         tagName: 'acoustic',
-        fileName: 'public/upbeat.mp3'
+        fileName: 'uploads/upbeat.mp3'
     },
     {
         id: '20',
         tagName: 'Upbeat',
-        fileName: 'public/upbeat.mp3'
+        fileName: 'uploads/upbeat.mp3'
     }
 ]
 
